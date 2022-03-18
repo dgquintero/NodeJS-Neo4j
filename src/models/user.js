956c9dff11c9bbc84e411b5eede74aa7b4f71afe
Daniel Quintero <dgquintero02@hotmail.com>
2022-03-17 23:09:09 -0500

    get data

--- src/models/user.js
+++ src/models/user.js
@@ -3,13 +3,22 @@ const neo4j = require('neo4j-driver');
 require('dotenv').config()
 const {
   url,
-  username,
-  password,
+  db_username,
+  db_password,
   database
 } = process.env
-const driver = neo4j.driver(url, neo4j.auth.basic(username, password));
+
+const driver = neo4j.driver(url, neo4j.auth.basic(db_username, db_password));
 
 const session = driver.session({ database });
 
-// session.run(query)
 
+const findAll = async ()=> {
+  const result = await session.run(`MATCH (n1) RETURN n1 LIMIT 25`)
+  console.log(result)
+  return result.records.map(i=>i.get('n1').properties)
+}
+
+export default {
+  findAll
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,27 +1,28 @@
 import { Router } from "express";
+import userModel from '../models/user'
 
 const user = Router()
 
-user.get('/', (req,res)=> {
-  const result = await user.findAll()
+user.get('/', async (req,res)=> {
+  const result = await userModel.findAll()
   res.json(result)
 })
-user.get('/:id', (req,res)=> {
-  const result = await user.findById()
-  res.json(result)
-})
-user.post('/', (req,res)=> {
-  const result = await user.create(req.body)
-  res.json(result)
-})
-user.put('/:id', (req,res)=> {
-  const result = await user.findByIdAndUpdate()
-  res.json(result)
-})
+// user.get('/:id', (req,res)=> {
+//   const result = await user.findById()
+//   res.json(result)
+// })
+// user.post('/', (req,res)=> {
+//   const result = await user.create(req.body)
+//   res.json(result)
+// })
+// user.put('/:id', (req,res)=> {
+//   const result = await user.findByIdAndUpdate()
+//   res.json(result)
+// })
 
-user.delete('/:id', (req,res)=> {
-  const result = await user.findByIdAndDelete()
-  res.json(result)
-})
+// user.delete('/:id', (req,res)=> {
+//   const result = await user.findByIdAndDelete()
+//   res.json(result)
+// })
 
 export default user
